70715e2ae6810e7e9169ef801989ec8fea87d3a0
Eduard Coll <ecollvila@gmail.com>
2017-06-07 22:37:25 +0200

    Rename the SpockSpecTest to SpockDTOSpecTest.

--- src/test/java/SpockDTOSpecTest.groovy
+++ src/test/java/SpockDTOSpecTest.groovy
@@ -4,7 +4,7 @@ import static org.spockframework.util.Assert.notNull
 /**
  * Created by hocklo on 7/06/17.
  */
-class SpockSpecTest extends Specification{
+class SpockDTOSpecTest extends Specification{
     def "Testing the SpockDTO with Spock"(){
         setup:
         def SpockDTO spockDTO = new SpockDTO()
